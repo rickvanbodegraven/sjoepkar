825fe833ed3d98ddded62bba61a08b366f5380f4
Rick van Bodegraven <rick@garagesoftware.nl>
2016-02-11 15:05:27 +0100

    First test for Danny, print the required info to stdout

--- main.go
+++ main.go
@@ -1,26 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"os"
-    "fmt"
 
-    "github.com/fsouza/go-dockerclient"
+	// import package under the alias "docker"
+	docker "github.com/fsouza/go-dockerclient"
 )
 
 func main() {
-	// retrieve the $DOCKER_HOST environment variable 
-    endpoint := os.Environ()["DOCKER_HOST"]
-	
-    client, _ := docker.NewClient(endpoint)
-	
-    imgs, _ := client.ListImages(docker.ListImagesOptions{All: false})
-	
-    for _, img := range imgs {
-        fmt.Println("ID: ", img.ID)
-        fmt.Println("RepoTags: ", img.RepoTags)
-        fmt.Println("Created: ", img.Created)
-        fmt.Println("Size: ", img.Size)
-        fmt.Println("VirtualSize: ", img.VirtualSize)
-        fmt.Println("ParentId: ", img.ParentID)
-    }
+	// retrieve the $DOCKER_HOST environment variable
+	endpoint := os.Getenv("DOCKER_HOST")
+
+	// initialize a docker-client connection to the unix socket
+	client, _ := docker.NewClient(endpoint)
+
+	// retrieve a list of all docker images
+	//	imgs, _ := client.ListImages(docker.ListImagesOptions{All: false})
+
+	// retrieve a list of all docker containers
+	containers, _ := client.ListContainers(docker.ListContainersOptions{})
+
+	// dump some info for all of these beautiful containers
+	for _, container := range containers {
+		fmt.Println("ID:", container.ID)
+		fmt.Println("Name:", container.Names)
+		fmt.Println("Label:", container.Labels)
+	}
 }
