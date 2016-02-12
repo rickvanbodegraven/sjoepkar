02cddd445251a75acb6eff475e97e236d34a94ea
Rick van Bodegraven <rick@garagesoftware.nl>
2016-02-12 13:03:45 +0100

    Print out the full name, minus the slash, of the proxy

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	// import package under the alias "docker"
 	docker "github.com/fsouza/go-dockerclient"
@@ -19,10 +20,8 @@ func main() {
 
 	// dump some info for all of these beautiful containers
 	for _, container := range containers {
-		fmt.Println(container.ID[:12], container.Names[0])
-
-		//		fmt.Println("ID:", container.ID)
-		//		fmt.Println("Name:", container.Names)
-		//		fmt.Println("Label:", container.Labels)
+		if strings.Contains(container.Names[0], "cssonline-proxy") {
+			fmt.Println(container.Names[1:])
+		}
 	}
 }
